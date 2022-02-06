c3edba83ad33e6d46c0d193a8712f5b66aa78c17
IsaacH <Isaac.hathaway28@gmail.com>
2022-02-06 09:28:35 -0700

    add handleclick function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const playerOne = -1
 const playerTwo = 1
 
 const winningCombos = [
-  [0, 1 ,2],
+  [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
   [0, 3, 6],
@@ -16,31 +16,17 @@ const winningCombos = [
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let isWinner, gameTie, guessSquare, playerOneGuesses, playerTwoGuesses, whoseTurn
+let boardArray, playerTurn, isWinner
 
-let boardArray = []
-let playerTurn = ""
 
 /*------------------------ Cached Element References ------------------------*/
 const messageEl = document.querySelector('#message')
-const square0 = document.querySelector('#sq0')
-const square1 = document.querySelector('#sq1')
-const square2 = document.querySelector('#sq2')
-const square3 = document.querySelector('#sq3')
-const square4 = document.querySelector('#sq4')
-const square5 = document.querySelector('#sq5')
-const square6 = document.querySelector('#sq6')
-const square7 = document.querySelector('#sq7')
-const square8 = document.querySelector('#sq8')
 const boardSect = document.querySelector('.board')
 const replayBtn = document.querySelector('#replay-button')
-const startBtn = document.querySelector('#start-button')
 const allSquares = document.querySelectorAll('.squares')
 
 console.log(replayBtn)
-console.log(startBtn);
 console.log(boardSect);
-console.log(square0);
 console.log(allSquares);
 /*----------------------------- Event Listeners -----------------------------*/
 
@@ -55,60 +41,69 @@ boardSect.addEventListener('click', handleClick)
 /*-------------------------------- Functions --------------------------------*/
 init()
 
-function init (){
+function init() {
   //show empty squares
-  boardArray = [1, null, null, null, null, null, null, null, null]
-  
+  boardArray = [null, null, null, null, null, null, null, null, null]
+  console.log(boardArray)
   //starting turn
-let playerTurn = 1 // 1 is player "X" and 0 is player "O"
-// winner
-isWinner = null
-//hide reset button
-replayBtn.setAttribute("hidden", true)
-//hide messageEl text
-messageEl.textContent = ""
-//render function
-render()
+  playerTurn = 1 // 1 is player "X" and 0 is player "O"
+  // winner
+  isWinner = null
+  //hide reset button
+  replayBtn.setAttribute("hidden", true)
+  //hide messageEl text
+  messageEl.textContent = ""
+  //render function
+  render()
 
 }
 
-function render(){
-  
+function render() {
+  let squareColor
   let squarePlaceholder
-  
-    boardArray.forEach((square, idx) => {
-      if(square === 1 ){ 
-        squarePlaceholder = "x"
-      } else if(square === -1 ) {
-        squarePlaceholder = "o"
-      } else {
-        squarePlaceholder = null
-      }
-      
-    allSquares[idx].textContent = squarePlaceholder
-    console.log(squarePlaceholder);
-    
-    
+
+  boardArray.forEach((square, idx) => {
+    if (square === 1) {
+      squareColor = "red"
+      squarePlaceholder = "X"
+    } else if (square === -1) {
+      squareColor = "blue"
+      squarePlaceholder = "O"
+    } else if (square === null) {
+      squareColor = "grey"
+      squarePlaceholder = null
+    }
+    allSquares[idx].style.color = squareColor
+    allSquares[idx].innerText = squarePlaceholder
   })
-  renderWin()
-
-};
-
-function handleClick(evt){
-  console.log(evt.target)
-}
-
-function renderState(){
-
-  if(renderWin === "T") {
+  if (!isWinner){
+    messageEl.textContent = `It is ${playerTurn === 1 ? "X" : "O"} Turn`
+  } else if (isWinner === "T") {
     messageEl.textContent = "This one was a Scrath! Replay see who is better: X's or O's?"
   } else {
-    messageEl.textContent = `Congratulations, ${Winner} you win!`
+    messageEl.textContent = `Congratulations, ${isWinner === 1 ? "X" : "O"} you win!`
   }
+  getWinner()
+};
+
+function handleClick(evt) {
+  let squareIndex = parseInt(evt.target.id.replace('sq', ''))
+
+  if (squareIndex === -1 || squareIndex === 1) {
+    return
+  } else if (isWinner !== null) {
+    return
+  }
+  // this tells populates the index to the player that clicked: 1 or -1
+  boardArray[squareIndex] = playerTurn
+  // alternate turns
+  // multiplying by -1 will alternate between -1 and 1
+  playerTurn = playerTurn * -1
+  
+  isWinner = getWinner
+  render()
+  console.log(boardArray)
 }
 
 
 
-function renderWin(){
-
-}
