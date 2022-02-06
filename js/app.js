d4953b694f6149817e5673d99e8ba6c1a0eba4d1
IsaacH <Isaac.hathaway28@gmail.com>
2022-02-06 10:01:33 -0700

    add getWinner function and make minor edits

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 /*-------------------------------- Constants --------------------------------*/
-const playerOne = -1
-const playerTwo = 1
 
 const winningCombos = [
   [0, 1, 2],
@@ -21,18 +19,19 @@ let boardArray, playerTurn, isWinner
 
 /*------------------------ Cached Element References ------------------------*/
 const messageEl = document.querySelector('#message')
-const boardSect = document.querySelector('.board')
+
 const replayBtn = document.querySelector('#replay-button')
 const allSquares = document.querySelectorAll('.squares')
 
-console.log(replayBtn)
-console.log(boardSect);
+console.log(replayBtn);
 console.log(allSquares);
 /*----------------------------- Event Listeners -----------------------------*/
 
-boardSect.addEventListener("reset", init)
+replayBtn.addEventListener("click", init)
 
-boardSect.addEventListener('click', handleClick)
+allSquares.forEach((square) => {
+square.addEventListener('click', handleClick)
+})
 
 // boardSect.addEventListener("button"), function(evt) {
 //   evt.preve
@@ -49,61 +48,82 @@ function init() {
   playerTurn = 1 // 1 is player "X" and 0 is player "O"
   // winner
   isWinner = null
-  //hide reset button
   replayBtn.setAttribute("hidden", true)
-  //hide messageEl text
-  messageEl.textContent = ""
   //render function
   render()
 
 }
 
 function render() {
-  let squareColor
-  let squarePlaceholder
+  
 
   boardArray.forEach((square, idx) => {
+    let squareColor
+    let squareLetter
     if (square === 1) {
       squareColor = "red"
-      squarePlaceholder = "X"
+      squareLetter = "X"
     } else if (square === -1) {
       squareColor = "blue"
-      squarePlaceholder = "O"
+      squareLetter = "O"
     } else if (square === null) {
-      squareColor = "grey"
-      squarePlaceholder = null
+      squareColor = "white"
+      squareLetter = null
     }
     allSquares[idx].style.color = squareColor
-    allSquares[idx].innerText = squarePlaceholder
+    allSquares[idx].innerText = squareLetter
   })
+
+
   if (!isWinner){
-    messageEl.textContent = `It is ${playerTurn === 1 ? "X" : "O"} Turn`
+    messageEl.innerText = `It is ${playerTurn === 1 ? "X's" : "O's"} turn`
   } else if (isWinner === "T") {
-    messageEl.textContent = "This one was a Scrath! Replay see who is better: X's or O's?"
+    messageEl.innerText = "This one was a Scrath! Replay see who is better: X's or O's?"
   } else {
     messageEl.textContent = `Congratulations, ${isWinner === 1 ? "X" : "O"} you win!`
   }
-  getWinner()
 };
 
 function handleClick(evt) {
   let squareIndex = parseInt(evt.target.id.replace('sq', ''))
 
-  if (squareIndex === -1 || squareIndex === 1) {
-    return
-  } else if (isWinner !== null) {
+  // if (squareIndex === -1 || squareIndex === 1) {
+  //   return
+  // } else if (isWinner !== null) {
+  //   return
+  // }
+
+  if (boardArray[squareIndex] || isWinner) {
     return
   }
+  replayBtn.removeAttribute("hidden")
   // this tells populates the index to the player that clicked: 1 or -1
   boardArray[squareIndex] = playerTurn
   // alternate turns
   // multiplying by -1 will alternate between -1 and 1
-  playerTurn = playerTurn * -1
+  playerTurn *= -1
   
-  isWinner = getWinner
+  isWinner = getWinner()
+
   render()
   console.log(boardArray)
 }
 
+function getWinner() {
 
+    if (Math.abs(boardArray[0] + boardArray[1] + boardArray[2]) === 3) return boardArray[0]
+    if (Math.abs(boardArray[3] + boardArray[4] + boardArray[5]) === 3) return boardArray[3]
+    if (Math.abs(boardArray[6] + boardArray[7] + boardArray[8]) === 3) return boardArray[6]
+    if (Math.abs(boardArray[0] + boardArray[3] + boardArray[6]) === 3) return boardArray[0]
+    if (Math.abs(boardArray[1] + boardArray[4] + boardArray[7]) === 3) return boardArray[1]
+    if (Math.abs(boardArray[2] + boardArray[5] + boardArray[8]) === 3) return boardArray[2]
+    if (Math.abs(boardArray[0] + boardArray[4] + boardArray[8]) === 3) return boardArray[0]
+    if (Math.abs(boardArray[2] + boardArray[4] + boardArray[6]) === 3) return boardArray[2]
+    
+    if (boardArray.includes(null)) {
+      return null
+    } else {
+      return "T"
+    }
 
+  }
